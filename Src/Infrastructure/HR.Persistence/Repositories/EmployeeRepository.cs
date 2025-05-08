79c39184237ae5c6ba48579ddfd850f533587ca3
Rahul Suthar <90858008+Rahulcs27@users.noreply.github.com>
2025-05-08 16:33:58 +0530

    fixed other conflict after tejas merge

--- Src/Infrastructure/HR.Persistence/Repositories/EmployeeRepository.cs
+++ Src/Infrastructure/HR.Persistence/Repositories/EmployeeRepository.cs
@@ -156,7 +156,6 @@ namespace HR.Persistence.Repositories
             };
         }
 
-<<<<<<< HEAD
 
 
 
@@ -172,7 +171,7 @@ namespace HR.Persistence.Repositories
 
 
             return employee.FirstOrDefault();
-=======
+        }
         public async Task<bool> UpdateEmployeeAsync(UpdateEmployeeCommandDto dto)
         {
             Console.WriteLine($"[DEBUG] Starting UpdateEmployeeAsync for Code: {dto.Code}");
@@ -249,7 +248,7 @@ namespace HR.Persistence.Repositories
                 Console.WriteLine($"[ERROR] Exception in UpdateEmployeeAsync: {ex.Message}");
                 return false;
             }
->>>>>>> dev
+
         }
     }
 }
