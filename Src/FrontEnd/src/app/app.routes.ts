46a4c761ac8e4508d650efbc995b4d57e75ca157
VaishnaviBhambure <vaishnavi.bhambure@neosoftmail.com>
2025-05-27 18:39:16 +0530

    Half Notification

--- Src/FrontEnd/src/app/app.routes.ts
+++ Src/FrontEnd/src/app/app.routes.ts
@@ -1,4 +1,4 @@
-import { Routes } from '@angular/router';
+ import { Routes } from '@angular/router';
 import { DashboardComponent } from './features/Dashboard/dashboard/dashboard.component';
 import { LoginComponent } from './features/login/login.component';
 import { OtpComponent } from './features/otp/otp.component';
