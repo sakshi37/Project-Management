0127f0ee68e14b21418821ba45a3f03c00afebd2
VaishnaviBhambure <vaishnavi.bhambure@neosoftmail.com>
2025-05-29 16:01:37 +0530

    Notificatioin n inactive mul

--- Src/FrontEnd/src/app/services/notification-service.ts
+++ Src/FrontEnd/src/app/services/notification-service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { NotificationModel, ReadAndDelNotifications } from '../Models/notification-model';
 import { API_URL } from '../../constant';
 
@@ -9,6 +9,8 @@ import { API_URL } from '../../constant';
 })
 export class NotificationService {
   private baseUrl = `${API_URL}/Notification`;
+    private unreadCountSubject = new BehaviorSubject<number>(0);
+  unreadCount$ = this.unreadCountSubject.asObservable();
 
   constructor(private http: HttpClient) {}
 
@@ -22,7 +24,16 @@ readAndDelNotifications(notificationId: number): Observable<{ message: string }>
     {}
   );
 }
-
-
-
+markAsRead(notificationId: number): Observable<any> {
+  return this.http.put(`${this.baseUrl}/make-read?notificationId=${notificationId}`,{});
+}
+  updateUnreadCount(code: string) {
+    this.getNotifications(code).subscribe({
+      next: (data) => {
+        const count = data.filter(n => !n.isRead).length;
+        this.unreadCountSubject.next(count); // 👈 this triggers the update
+      },
+      error: (err) => console.error('Error updating unread count', err)
+    });
+  }
 }
